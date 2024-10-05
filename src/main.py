fbb6bf72cd01ee927d2eeb6a1d5118113844a239
nicollaswendel <nicollaswendel1623@gmail.com>
2024-10-05 10:46:31 -0300

    Modifica a função soma

--- src/main.py
+++ src/main.py
@@ -1,6 +1,2 @@
 def soma(a, b):
-    return a + b
-
-if __name__ == "__main__":
-    resultado = soma(3, 5)
-    print(f"O resultado é {resultado}")
+    return a + b + 1 # Introduzindo um erro proposital
